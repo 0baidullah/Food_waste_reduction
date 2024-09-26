c7c4fac25b90cd8c2d95bfc32454826618a7e22f
Obaidullah <132139450+0baidullah@users.noreply.github.com>
2024-09-26 13:39:01 +0100

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,10 @@
 // src/socket.js
 import io from "socket.io-client";
 
-const socket = io("http://localhost:8000"); 
+// const socket = io("http://localhost:8000"); 
+
+// for deploy
+const socket = io("https://food-waste-reduction-1.onrender.com"); 
+
 
 export default socket;
